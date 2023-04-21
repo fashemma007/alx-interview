4587568dfb8cee005cce3b27f72af9025fbcf105
Emmanuel Fasogba <51290946+fashemma007@users.noreply.github.com>
2023-04-21 19:30:27 +0100

    docs
    In line code documentations

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -6,17 +6,18 @@ def pascal_triangle(n):
     """
     returns a representation of the Pascal’s triangle of n
     """
-    triangle = []
+    triangle = []  # initialize empty list to return
     if n <= 0:  # return empty list when 0 is given
         return triangle
-    for i in range(n):  # loop thru
-        temp_list = []
-
+    for i in range(n):  # loop from 0 to n
+        temp_list = []  # initialize temporary list for each iteration
+        # sub list creation loop
         for j in range(i+1):
             if j == 0 or j == i:
-                temp_list.append(1)  # append to temporary list
-            else:
+                # append 1 at beginning and end of lists
+                temp_list.append(1)
+            else:  # add 2 opposite values to get new value
                 temp_list.append(triangle[i-1][j-1] + triangle[i-1][j])
-        triangle.append(temp_list)
+        triangle.append(temp_list)  # append temp list to return list
     # print(triangle)
     return triangle
