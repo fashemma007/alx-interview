fda59395c92f36575c6753bbe1f9f45247f0f891
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-04-26 10:55:52 +0100

    feat[interview-2]: lockboxes algorithm

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,27 @@
+def canUnlockAll(boxes):
+    """Determines if all boxes can be unlocked"""
+    unlocked = [0]  # store all boxes that's been unlocked
+    # loop and get the index and content of each box i.e inner list
+    for box_index, box in enumerate(boxes):
+        # print(f"Keys in box {box_index} can unlock boxes => {box}")
+        if not box:  # if box is empty skip it
+            continue
+        for key in box:
+            # print(f"🔑 for box {key} found in 🥡 {box_index}")
+            # check if the key falls within box range
+            key_valid = (key < len(boxes))
+            # check if the key is not already in the list
+            key_missing = (key not in unlocked)
+            # check if the key isnt a duplicate to open current box
+            key_not_for_current_box = (key != box_index)
+            all_checks = key_valid and key_missing and key_not_for_current_box
+            # if all conditions are met
+            if all_checks:
+                # print(f"🥡 {key} can now be unlocked")
+                # add the key to the unlocked box
+                unlocked.append(key)
+                # print(unlocked)
+            # else:
+                # print(f"Duplicate 🔑 {key} found in 🥡 {box_index}")
+    # after all iterations, check if all boxes were unlocked
+    return True if len(unlocked) == len(boxes) else False
---
+++ 0x01-lockboxes/README.md
@@ -0,0 +1,17 @@
+<h1 style="text-align: center;">
+	<a href='https://intranet.alxswe.com/projects/1214'>
+		Lockboxes
+	</a>
+</h1>
+
+
+## Resources
+* [Lockboxes](https://stackoverflow.com/questions/63024056/lockboxes-problem-list-inside-a-list-and-each-list-contains-keys-to-unlock-the)
+
+---
+
+<h2 style="text-align: center;">Collaborative Author(s)</h2>
+
+[**Emmanuel Fasogba**](https://www.linkedin.com/in/emmanuelofasogba/)
+- GitHub - [fashemma007](https://github.com/fashemma007)
+- Twitter - [@tz_emiwest](https://www.twitter.com/tz_emiwest)
