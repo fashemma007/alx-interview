4d6d0cbaebc1931ae947bed512fbbe43193c073d
Emmanuel Fasogba <fasogbaemmanuel@gmail.com>
2023-05-04 22:08:56 +0100

    feat:

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""module docs for 0-minoperations.py"""
+
+
+def minOperations(n):
+    """Returns an integer"""
+    if n <= 1:
+        return 0
+    dp = [0] * (n + 1)
+    # print(dp)
+    for i in range(2, n + 1):
+        dp[i] = i
+        # print(dp)
+        for j in range(i - 1, 1, -1):
+            if i % j == 0:
+                dp[i] = dp[j] + (i // j)
+                break
+    return dp[n]
