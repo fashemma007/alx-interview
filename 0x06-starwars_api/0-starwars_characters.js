de9848866efa1bb9d4fe1e6ae34f5d7aaa1641ac
Emmanuel Fasogba <51290946+fashemma007@users.noreply.github.com>
2023-06-03 18:37:13 +0100

    docs

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const id = process.argv.slice(2);
+const id = process.argv.slice(2);  // cmd args
 const url = `https://swapi-api.hbtn.io/api/films/${id[0]}/`;
 
 const request = require('request');
@@ -7,15 +7,18 @@ request(url, async function (error, response, body) {
   if (error) {
     return console.error(error);
   }
+  // convert return body to json
   const bodyJson = JSON.parse(body);
+  // iterate through xters url in object
   for (const characters of bodyJson.characters) {
+    // fetch their details and wait for response
     await new Promise(function (resolve, reject) {
       request(characters, function (error, response, body) {
         if (error) {
           return console.error(error);
         }
-        const bdJson = JSON.parse(body);
-        console.log(bdJson.name);
+        const xterJson = JSON.parse(body);
+        console.log(xterJson.name);
         resolve();
       });
     });
